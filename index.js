889f4284c2adbb839095309e849a4b40a1d1191f
Steve Gomez <StevenAGomez@Live.com>
2021-11-04 12:40:53 -0700

    Adds config va remote

--- index.js
+++ index.js
@@ -5,32 +5,22 @@ import {
   initGlobalState,
   loadMicroApp,
 } from 'qiankun';
+import axios from 'axios';
 import render from './render/ReactRender';
+
+axios
+  .get('/config.json')
+  .then((res) => {
+    console.log('[platform-root] Initializing application');
+    init({ config: res.data });
+  })
+  .catch((err) => {
+    console.log('catch', err);
+  });
 
 render({ loading: true });
 
 const loader = (loading) => render({ loading });
-
-// TODO: [platform-root] Temporary configuration options -- retrieve from remote
-const config = [
-  {
-    name: 'menu',
-    entry: '//localhost:7100',
-    container: '#menu',
-  },
-  {
-    name: 'mf-subapp1',
-    entry: '//localhost:7101',
-    container: '#viewport',
-    activeRule: '/mf-subapp1',
-  },
-  {
-    name: 'mf-subapp2',
-    entry: '//localhost:7102',
-    container: '#viewport',
-    activeRule: '/mf-subapp2',
-  },
-];
 
 // Retrieve menu app from configuration
 const getMenuApp = (config) => config.find((app) => app.name === 'menu');
@@ -44,56 +34,58 @@ const getChildApps = (config) =>
     return acc;
   }, []);
 
-// TODO: [platform-root] Determine if we need any lifecycle events
-registerMicroApps(getChildApps(config), {
-  beforeLoad: [
-    (app) => {
-      console.log(
-        '[platform-root] beforeLoad - %c%s',
-        'color: green;',
-        app.name,
-      );
+const init = ({ config }) => {
+  // TODO: [platform-root] Determine if we need any lifecycle events
+  registerMicroApps(getChildApps(config), {
+    beforeLoad: [
+      (app) => {
+        console.log(
+          '[platform-root] beforeLoad - %c%s',
+          'color: green;',
+          app.name,
+        );
+      },
+    ],
+    beforeMount: [
+      (app) => {
+        console.log(
+          '[platform-root] beforeMount - %c%s',
+          'color: green;',
+          app.name,
+        );
+      },
+    ],
+    afterUnmount: [
+      (app) => {
+        console.log(
+          '[platform-root] afterUnmount - %c%s',
+          'color: green;',
+          app.name,
+        );
+      },
+    ],
+  });
+
+  const { onGlobalStateChange, setGlobalState } = initGlobalState({
+    user: 'platform-root',
+  });
+
+  onGlobalStateChange((value, prev) =>
+    console.log('[platform-root] onGlobalStateChange - ', value, prev),
+  );
+
+  setGlobalState({
+    ignore: 'master',
+    user: {
+      name: 'master',
     },
-  ],
-  beforeMount: [
-    (app) => {
-      console.log(
-        '[platform-root] beforeMount - %c%s',
-        'color: green;',
-        app.name,
-      );
-    },
-  ],
-  afterUnmount: [
-    (app) => {
-      console.log(
-        '[platform-root] afterUnmount - %c%s',
-        'color: green;',
-        app.name,
-      );
-    },
-  ],
-});
+  });
 
-const { onGlobalStateChange, setGlobalState } = initGlobalState({
-  user: 'platform-root',
-});
+  loadMicroApp(getMenuApp(config));
 
-onGlobalStateChange((value, prev) =>
-  console.log('[platform-root] onGlobalStateChange - ', value, prev),
-);
+  start();
 
-setGlobalState({
-  ignore: 'master',
-  user: {
-    name: 'master',
-  },
-});
-
-loadMicroApp(getMenuApp(config));
-
-start();
-
-runAfterFirstMounted(() => {
-  console.log('[platform-root] runAfterFirstMounted');
-});
+  runAfterFirstMounted(() => {
+    console.log('[platform-root] runAfterFirstMounted');
+  });
+};
